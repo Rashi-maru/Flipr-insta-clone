43741143069d32773446d452f047ca493c21b604
Rashi-maru <70251316+Rashi-maru@users.noreply.github.com>
2022-02-10 10:39:31 +0530

    Update server.js

--- clone-instagram/email_service/server.js
+++ clone-instagram/email_service/server.js
@@ -1,7 +1,7 @@
 const express = require("express"),
     morgan = require("morgan");
 
-const { PORT, MODE } = require("./config");
+const { PORT, MODE, SERVICETOKEN } = require("./config");
 const { connect } = require("./api/v1/helper/dbconnect");
 const emailRouter = require("./api/v1/email.router");
 
@@ -9,14 +9,20 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 // routes
-app.use("/", emailRouter);
+app.use((req, res, next) => {
+    if (req.headers["authorization"] !== SERVICETOKEN) {
+        return res.sendStatus(401);
+    }
+    next();
+});
 
-app.get("/ping", (req, res) => {
+app.use("/", emailRouter);
+app.get("/ping", (_req, res) => {
     res.status(200).send("pong");
 });
 
 // dead end
-app.use((req, res) => {
+app.use((_req, res) => {
     res.status(404).json({ message: "verb not supported" }).end();
 });
 
