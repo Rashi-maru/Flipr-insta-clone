cf4cf26ed760a5c2ea39f1635e2343a7d8cb4b31
Rashi-maru <70251316+Rashi-maru@users.noreply.github.com>
2022-02-10 10:42:55 +0530

    Update user.controller.js

--- clone-instagram/src/api/v1/controllers/user.controller.js
+++ clone-instagram/src/api/v1/controllers/user.controller.js
@@ -7,7 +7,7 @@ const {
     verificationCodeValidator,
 } = require("../validators/user.validator");
 
-exports.signup = async(req, res) => {
+exports.signup = async (req, res) => {
     try {
         const { email, password, username, name, ...rest } = req.body;
         if (!name) {
@@ -24,27 +24,32 @@ exports.signup = async(req, res) => {
         }
         if (!passwordValidator(password))
             return res.status(400).send({
-                message: "NOTE: Weak password so, therefore, try a combination of numbers/symbols and a good length",
+                message:
+                    "Weak password try a combination of numbers/symbols and a good length",
             });
 
         let user = await UserService.findOnebyEmailorUsername(username, email);
         if (user) {
             if (user.email === email && user.verifiedUser === true) {
                 return res.status(400).send({
-                    message: "The email already exists so try forgot password!",
+                    message:
+                        "Email already Exists Please proceed to forgot password!",
                 });
             } else if (user.email === email) {
-                user = await UserService.findOneAndUpdate({ email }, {
-                    username,
-                    email,
-                    name,
-                    ...rest,
-                    passwordHash: password,
-                    verifiedUser: false,
-                }, );
+                user = await UserService.findOneAndUpdate(
+                    { email },
+                    {
+                        username,
+                        email,
+                        name,
+                        ...rest,
+                        passwordHash: password,
+                        verifiedUser: false,
+                    },
+                );
             } else if (user.username === username) {
                 return res.status(400).send({
-                    message: "User Name already Exists!",
+                    message: "User Name already Exists !!",
                 });
             }
         }
@@ -73,13 +78,14 @@ exports.signup = async(req, res) => {
         );
 
         return res.status(201).send({
-            message: "Check your email. We have sent you a mail.",
+            message:
+                "We just sent an email to your account. Confirm your mail to login",
         });
     } catch (e) {
         return res.status(500).json({ message: e.message });
     }
 };
-exports.verifyEmailLink = async(req, res) => {
+exports.verifyEmailLink = async (req, res) => {
     try {
         let { id } = req.params;
         if (!verificationCodeValidator(id)) {
@@ -99,7 +105,7 @@ exports.verifyEmailLink = async(req, res) => {
         return res.status(500).json({ message: e.message });
     }
 };
-exports.searchUsers = async(req, res) => {
+exports.searchUsers = async (req, res) => {
     const { searchBy, searchValue, limit } = req.body;
     if (!searchBy || !searchValue) {
         return res
@@ -119,7 +125,7 @@ exports.searchUsers = async(req, res) => {
         return res.status(500).json([]);
     }
 };
-exports.findUserByusername = async(req, res) => {
+exports.findUserByusername = async (req, res) => {
     try {
         const { id } = req.params;
         if (!id) return res.status(400).send();
@@ -136,7 +142,7 @@ exports.findUserByusername = async(req, res) => {
     }
 };
 
-exports.updateCurrentUser = async(req, res) => {
+exports.updateCurrentUser = async (req, res) => {
     try {
         const update = {};
         [
@@ -145,7 +151,7 @@ exports.updateCurrentUser = async(req, res) => {
             "password",
             "phoneNumber",
             "profileStatus",
-            "Notification",
+            "notification",
         ].forEach((element) => {
             if (req.body[element]) {
                 update[element] = req.body[element];
@@ -159,7 +165,7 @@ exports.updateCurrentUser = async(req, res) => {
     }
 };
 
-exports.forgotPassword = async(req, res) => {
+exports.forgotPassword = async (req, res) => {
     try {
         const { email } = req.body;
         if (!email || !emailValidator(email))
@@ -184,7 +190,7 @@ exports.forgotPassword = async(req, res) => {
         return res.status(500).json({ message: e.message });
     }
 };
-exports.updateforgotPassword = async(req, res) => {
+exports.updateforgotPassword = async (req, res) => {
     try {
         const { code, password } = req.body;
         if (!verificationCodeValidator(code)) {
@@ -211,6 +217,6 @@ exports.updateforgotPassword = async(req, res) => {
         return res.status(500).json({ message: e.message });
     }
 };
-exports.getUserProfile = async(req, res) => {
+exports.getUserProfile = async (req, res) => {
     return res.json(req.user);
 };
